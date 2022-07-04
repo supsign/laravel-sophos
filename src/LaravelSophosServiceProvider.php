4675e435dc9a1bd865a5cf2197fcc802c8861e1e
Florian Ratz <florian.ratz@supsign.ch>
2022-07-04 08:40:37 +0200

    init

---
+++ src/LaravelSophosServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Supsign\LaravelSophos;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelSophosServiceProvider extends ServiceProvider
+{
+    public function boot() 
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
---
+++ src/SophosApi.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Supsign\LaravelSophos;
+
+class SophosApi
+{
+
+}
